2f684f1278fd59e813ea6389629dfbd48512b940
mdhtsami101 <tamanko00@gmail.com>
2024-10-28 22:27:37 +0300

    add font styles

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveapp/widgets/adaptive_layout.dart';
+import 'package:responsiveapp/widgets/dashboard_desktop_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
   }
 }
