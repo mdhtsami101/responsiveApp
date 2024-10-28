7d14ffe0bd7d1dc2faf2b478b5f69f7ac4595406
mdhtsami101 <tamanko00@gmail.com>
2024-10-29 01:25:19 +0300

    finish custom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsiveapp/models/drawer_item_model.dart';
 import 'package:responsiveapp/utils/app_images.dart';
+import 'package:responsiveapp/widgets/active_and_inactive_item.dart';
 import 'package:responsiveapp/widgets/darwer_items_list_view.dart';
 import 'package:responsiveapp/widgets/drawer_item.dart';
 import 'package:responsiveapp/widgets/user_info_list_tile.dart';
@@ -12,17 +13,39 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          const UserInfoListTile(
-            image: Assets.imagesAvatar3,
-            title: "Lekan Okeowo",
-            subtitle: "demo@gmail.com",
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              image: Assets.imagesAvatar3,
+              title: "Lekan Okeowo",
+              subtitle: "demo@gmail.com",
+            ),
           ),
-          const SizedBox(
-            height: 8,
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
           ),
-          const DrawerItemsListView(),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                InActiveDrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                        title: 'Setting system', image: Assets.imagesSettings)),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Logout account', image: Assets.imagesLogout),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
