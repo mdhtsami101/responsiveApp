b79c746ac37ce25e3d2a0d32ee960fb75f1bdf8e
mdhtsami101 <tamanko00@gmail.com>
2024-10-29 02:42:28 +0300

    start in all expenses

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveapp/widgets/all_expensess.dart';
 import 'package:responsiveapp/widgets/custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -9,6 +10,14 @@ class DashboardDesktopLayout extends StatelessWidget {
     return const Row(
       children: [
         Expanded(child: CustomDrawer()),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          child: Column(
+            children: [Expanded(child: AllExpenses())],
+          ),
+        ),
       ],
     );
   }
