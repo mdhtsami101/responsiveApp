dedc04a0ed28c2f6f68a77220fe810cc1099ffad
mdhtsami101 <tamanko00@gmail.com>
2024-10-28 23:50:21 +0300

    add user info listtile

---
+++ lib/widgets/user_info_list_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsiveapp/utils/app_images.dart';
+import 'package:responsiveapp/utils/app_styles.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subtitle});
+  final String image, title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xFFFAFAFA),
+      elevation: 0,
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: AppStyles.styleSemiBold16,
+        ),
+        subtitle: Text(
+          subtitle,
+          style: AppStyles.styleRegular12,
+        ),
+      ),
+    );
+  }
+}
