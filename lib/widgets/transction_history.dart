051d0ae0dfc94d5d3e11a877ffc2c97c343d0fee
mdhtsami101 <tamanko00@gmail.com>
2024-11-21 22:36:50 +0200

    transaction part tow

--- lib/widgets/transction_history.dart
+++ lib/widgets/transction_history.dart
@@ -6,21 +6,40 @@ class TransactionHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(
-              'Transaction History',
-              style: AppStyles.styleSemiBold20,
-            ),
-            Text(
-              'See all',
-              style: AppStyles.styleMedium16
-                  .copyWith(color: const Color(0XFF4EB7F2)),
-            ),
-          ],
+        const TransactionHistoryHeader(),
+        SizedBox(
+          height: 20,
+        ),
+        const Text(
+          '13 April 2022',
+          style: AppStyles.styleRegular14,
+        ),
+      ],
+    );
+  }
+}
+
+class TransactionHistoryHeader extends StatelessWidget {
+  const TransactionHistoryHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'Transaction History',
+          style: AppStyles.styleSemiBold20,
+        ),
+        Text(
+          'See all',
+          style:
+              AppStyles.styleMedium16.copyWith(color: const Color(0XFF4EB7F2)),
         ),
       ],
     );
